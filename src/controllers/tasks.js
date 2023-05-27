256fd707ae306a1c04fa9c1ffc70c4962cf25a9b
Jonathan Kechian <jkechian@gmail.com>
2023-05-26 21:21:49 -0300

    mod

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -3,7 +3,7 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.find({})
-        res.status(201).json({ tasks })    
+        res.status(200).json({tasks})    
     } catch (error) {
         res.status(500).json({ msg: error.message })
     }
